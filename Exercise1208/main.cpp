de0116c76daceb2709d8a15587e019de3aeb11f7
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-10-04 17:18:24 -0700

    Initial Commit

---
+++ Exercise1208/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Exercise1208
+//
+//  Created by Jacob Yeung on 10/4/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
