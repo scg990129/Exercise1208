13fe24342f292e0bf10c39174b654557380604c3
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-10-04 22:02:55 -0700

    add file

---
+++ Exercise1208/arrayListType.cpp
@@ -0,0 +1,50 @@
+//
+//  arrayListType.cpp
+//  Exercise1208
+//
+//  Created by Jacob Yeung on 10/4/23.
+//
+
+#include "unorderedArrayListType.hpp"
+#include "arrayListType.hpp"
+#include <iostream>
+
+arrayListType::arrayListType(int size): unorderedArrayListType(size) {
+}
+
+arrayListType::~arrayListType() {
+    unorderedArrayListType::~unorderedArrayListType();
+}
+
+void arrayListType::removeAll(){
+    unorderedArrayListType::removeAll();
+}
+
+void arrayListType::print(){
+    unorderedArrayListType::print();
+}
+
+void arrayListType::remove() {
+    unorderedArrayListType::remove();
+}
+void arrayListType::remove(int item) {
+    unorderedArrayListType::remove(item);
+}
+
+void arrayListType::insert(int item) {
+    for(int i = 0; i < length; i++){
+        if(list[i] == item){
+            return;
+        }else{
+            if(item < this->list[i]){
+                for(int j = length; j > i; j--){
+                    list[j] = list[j - 1];
+                }
+                list[i] = item;
+                length++;
+                return;
+            }
+        }
+    }
+    unorderedArrayListType::insert(item);
+}
---
+++ Exercise1208/arrayListType.hpp
@@ -0,0 +1,30 @@
+//
+//  arrayListType.hpp
+//  Exercise1208
+//
+//  Created by Jacob Yeung on 10/4/23.
+//
+
+#ifndef arrayListType_hpp
+#define arrayListType_hpp
+
+#include <stdio.h>
+
+#include "unorderedArrayListType.hpp"
+
+class arrayListType : public unorderedArrayListType {
+private:
+    int *list; // Pointer to the dynamic array
+    int maxSize; // Maximum size of the list
+    int length; // Current length of the list
+public:
+    arrayListType(int size = 100); // Constructor with default size
+    ~arrayListType(); // Destructor
+    void insert(int item); // Insert an item at the end
+    void removeAll(); // Remove all occurrences of the item
+    void remove(); // Remove the first occurrence of the item
+    void remove(int item); // Remove the first occurrence of the item
+    void print(); // Print the list
+};
+
+#endif /* arrayListType_hpp */
